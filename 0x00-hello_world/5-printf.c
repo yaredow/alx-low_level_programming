5c024f38c6dfe013dfcacbd62714dd4f4c7e2e12
yaredow <yaredyilma11@gmail.com>
2021-08-26 04:50:07 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,6 @@
  * main: prints text
  */
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return 0;
 }
