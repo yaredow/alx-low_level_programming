ad3eef4c399b216709bdb014b1e3206e45fe2f72
yaredow <yaredyilma11@gmail.com>
2021-08-26 04:00:23 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+   char string[40];
+   strcpy(str,("Programming is like building a multilingual puzzle");
+   puts(string);
+   return 0;
+}
