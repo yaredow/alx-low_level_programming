91b324ab35b80e1e91925a89ff5957e9b6d4f6df
yaredow <yaredyilma11@gmail.com>
2021-08-26 04:52:25 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-  * main: - Prints a text
-  *
-  * Return: 0 is always
-  */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-i	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
