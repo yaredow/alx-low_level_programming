249d20b1b8e37ecc2ee5f146c10532a0db2ca377
yaredow <yaredyilma11@gmail.com>
2021-08-26 04:53:55 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+
 /*
  * main: prints text
  */
+
 int main(void)
-	printf("with proper grammar, but the outcome is a piece of art,");
+
+	printf("with proper grammar, but the outcome is a piece of art");
 	return 0;
+
 }
