e766968142323463de6d7e95e96e6e606ca39bf9
yaredow <yaredyilma11@gmail.com>
2021-08-26 05:02:20 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/*
- * main: prints text
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
