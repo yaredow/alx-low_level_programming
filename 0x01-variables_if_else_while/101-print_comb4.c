4ab3cbdb1369c7a5dd6dd52927bd31bf70bd384a
yaredow <yaredyilma11@gmail.com>
2021-08-27 11:08:25 -0700

    alphabet

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,27 +1,24 @@
-#include <stdio.h>
-
-/**
-  * main - Prints combination of numbers
-  *
-  * Return: Always (Success)
-  */
 int main(void)
 {
-	int c, i;
+	int c, i, k;
 
 	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
-			if (c < i)
+			for (k = '0'; k <= '9'; k++)
 			{
-				putchar(c);
-				putchar(i);
-
-				if (c != '8' || (c == '8' && i != '9'))
+				if (c < i && i < k)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(c);
+					putchar(i);
+					putchar(k);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
