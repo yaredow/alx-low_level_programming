e0cccff117a10f96dfdd5273ffb9549e8d0e008c
yaredow <yaredyilma11@gmail.com>
2021-08-31 04:15:48 -0700

    putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,22 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* main - Print the word "Holberton"
-*
-* Return: Always 0 (Success)
-*/
-
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int i = 0;
-	char p[] = "Holberton";
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
-	while  (i <= 9)
-		{
-		_putchar(p[i]);
-		p[i]++;
-		}
 	return (0);
 }
-
