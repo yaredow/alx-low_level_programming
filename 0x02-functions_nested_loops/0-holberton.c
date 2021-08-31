572b276a8f277a5c750e10e7045f35394eb57db3
yaredow <yaredyilma11@gmail.com>
2021-08-31 04:01:08 -0700

    putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
 	_putchar('H');
