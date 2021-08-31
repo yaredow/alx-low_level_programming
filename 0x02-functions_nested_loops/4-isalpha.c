013fbe49be73869f7158cbc1ba34619974a9cd2d
yaredow <yaredyilma11@gmail.com>
2021-08-31 09:07:05 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,12 @@
 #include <main.h>
 
+/**
+ *
+ * _isalpha - check weather input is alphabet or not 
+ * Return: 1 for it is alphabet and 0 for it is not
+ *
+ */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z' ) || (c >= 'A' && c <= 'Z'))
