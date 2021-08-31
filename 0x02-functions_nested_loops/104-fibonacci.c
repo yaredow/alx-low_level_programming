e7baaf7efb110caa2f893b9f58d9ebf3352e9fbd
yaredow <yaredyilma11@gmail.com>
2021-08-31 11:45:36 -0700

    time table

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,48 +1,48 @@
+w#include "main.h"
 #include <stdio.h>
-#define Z 1000000000
 /**
- * main - increasing and decreasing until 98
- *
- * Return: Always 0.
- */
+  * main - prints first 98 fibbonacci nums
+  *
+  *
+  * Return: 0
+  */
 int main(void)
 {
-unsigned long int a, a1, a2, b, b1, b2, r, r1, r2, i;
-
-a = 1;
-b = 2;
-r = a + b;
-
-printf("%lu, %lu,", a, b);
-
-for (i = 0; i < 86; i++)
-{
-printf("%lu", r);
-a = b;
-b = r;
-r = a + b;
-}
-
-b1 = b / Z;
-b2 = b % Z;
-r1 = r / Z;
-r2 = r % Z;
-
-for (i = 0; i < 9; i++)
-{
-printf("%lu%lu,", r1, r2);
-a1 = b1;
-a2 = b2;
-b1 = r1;
-b2 = r2;
-
-r1 = a1 + b1 + (a2 + b2) / Z;
-r2 = (a2 + b2) % Z;
-}
-
-printf("%lu%lu\n", r1, r2);
-
-return (0);
-
+	/* declarations */
+	int i = 3;
+	int large = 0;
+	unsigned long a = 1, b = 2, term_1 = 0, a_2 = 0, b_2 = 0, term_2 = 0;
+	unsigned long cut = 10000000000000000;
+	/*printing the first nums */
+	printf("%lu, %lu", a, b);
+	/* looping up to 98 */
+	for (; i < 99; i++)
+	{
+		/* prints comma space */
+		printf(", ");
+		/* dissecting large nums */
+		term_1 = a + b;
+		if (term_1 > cut)
+			large = 1;
+		/*printing large nums */
+		if (large)
+		{
+			term_2 = term_1 / cut + a_2 + b_2;
+			term_1 = term_1 % cut;
+			printf("%lu", term_2);
+			printf("%016lu", term_1);
+			a_2 = b_2;
+			b_2 = term_2;
+		}
+		/*printing regular nums */
+		else
+		{
+			printf("%lu", term_1);
+		}
+		a = b;
+		b = term_1;
+	}
+	printf("\n");
+	return (0);
 }
 
