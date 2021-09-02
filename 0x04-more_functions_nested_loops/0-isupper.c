840c51fb5c97243dab687073624f9ed40220a6b6
yaredow <yaredyilma11@gmail.com>
2021-09-02 02:17:37 -0700

    more function, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
   * _isupper - Check if a letter is upper
-  * @x: The number to be checked
+  * @c: The number to be checked
   *
   * Return: 1 for upper letter or 0 for any else
   */
