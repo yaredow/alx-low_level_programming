aca67fc009071122c77da1adf41d48a522449525
yaredow <yaredyilma11@gmail.com>
2021-09-02 04:07:59 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -12,4 +12,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-#endif 
+#endif /* MAIN_H */ 
