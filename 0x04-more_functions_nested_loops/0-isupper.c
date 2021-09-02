499be5a54e9fb61e17fad9dc4991e0a28416071f
yaredow <yaredyilma11@gmail.com>
2021-09-02 02:07:09 -0700

    more function, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,12 +8,12 @@
  *
  */
 
-int _isupper(int c) {
+int  _isupper(int c) 
+{
 	if (c >= 65 && c <= 122)
 	{
 		return (1);
 	}
-	else {
 		return (0);
-	}
+
 }
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * _isdigit - checks for digit
+ * @c: a character
+ *
+ * Return: returns 1 if c is digit and don't if it is not
+ *
+ */
+int _isdigit(int c)
+{
+	for (c=0; c<=9; c++)
+	{
+		while
