a3f9efd155e5800243ca41bb472ba2a40b7d96b6
yaredow <yaredyilma11@gmail.com>
2021-09-02 05:47:41 -0700

    except

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+i#include "main.h"
+
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{
+	int leng;
+
+	if (n > 0)
+	{
+		for (leng = 0; leng < n; len++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
+
