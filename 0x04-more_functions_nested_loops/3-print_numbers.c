0acc6bf2d38e40394e4d651627ffd2afcc3f5238
yaredow <yaredyilma11@gmail.com>
2021-09-02 04:26:58 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ * print_numbers - prints number from 0-9
+ *
+ */
+
+void print_numbers(void)
+{
+	for (int i=0; i<=9; i++)
+	{
+		_putchar(i);
+		_putchar(\n);
+	}
+	return (0);
+}
+
