333473e2d75a53307c24284df904a98f0ca633a1
yaredow <yaredyilma11@gmail.com>
2021-09-06 07:52:06 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 - update the value it points to
- *
- *@n: the pointer to an int 
+ * reset_to_98 - main
+ * @n: - pinter to int
  */
 
 void reset_to_98(int *n)
-{	
-	*n = 98; 
+{
+*n = 98;
 }
-
-
