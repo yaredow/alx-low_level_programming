8660c93ffba0742337ba58063043e67648a96eb5
yaredow <yaredyilma11@gmail.com>
2021-09-08 22:10:13 -0700

    more pointers, arrays and string

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,35 +1,27 @@
+
 #include "holberton.h"
-void print_unsigned_int(unsigned int n);
+
 /**
- * print_number - print number n with putchar
- *
- * @n: number to print
- *
- * Return: always void
- */
+  * print_number - print numbers chars
+  * @n: integer params
+  * Return: 0
+ **/
+
 void print_number(int n)
 {
+	unsigned int n1;
+
+	n1 = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		print_unsigned_int(-(unsigned int)n);
+		n1 = -n;
 	}
-	else
-		print_unsigned_int(n);
-}
-/**
- * print_unsigned_int - prints an unsigned integer
- *
- * @n: an unsigned integer to print
- *
- * Return: always void
- */
-void print_unsigned_int(unsigned int n)
-{
-	if (n / 10 != 0)
-	{
-		print_unsigned_int(n / 10);
-	}
-	_putchar((n % 10) + '0');
-}
 
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
