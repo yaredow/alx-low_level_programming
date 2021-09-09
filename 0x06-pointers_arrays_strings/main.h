009ee69523ce43d3f505081b843a3d7b09bd679b
Yared Yilma <87979010+yaredow@users.noreply.github.com>
2021-09-09 09:08:20 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,18 +1,31 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-#include <stddef.h>
+char *_strcat(char *dest, char *src);
+
+void print_number(int n);
 
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
-void print_number(int n)
+
+void print_buffer(char *b, int size);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
+
+char *rot13(char *);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 #endif
 
