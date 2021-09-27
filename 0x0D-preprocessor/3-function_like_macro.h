da17c43866d9407b2973139cd0b80b98f3fa9d2e
yaredow <yaredyilma11@gmail.com>
2021-09-27 10:09:41 -0700

    Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,7 +3,6 @@
 
 /*
  * File: 3-function_like_macro.h
- * Auth: Brennan D Baraban
  * Desc: Header file that defines a function-like macro ABS(x)
  *       that computes the absolute value of a number x.
  */
