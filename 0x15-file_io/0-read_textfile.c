06967b5714d934e2c34c70cfc8af4e72fcd155b1
yaredow <yaredyilma11@gmail.com>
2021-10-18 11:25:20 -0700

    File input an output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,47 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+
 /**
- * read_textfile - reads a certain amount of letters from a file
- * @filename: name of file
- * @letters: how many letters to read
- * Return: the number of letters
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ *
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/* declarations */
-	int fd, retVal, writeVal;
-	char *buffer = malloc(sizeof(char) * letters);
+	int fd;
+	int s, t;
+	char *buf;
 
-	if (buffer == NULL || filename == NULL)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
+	s = read(fd, buf, letters);
+	if (s < 0)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
-	retVal = read(fd, buffer, letters);
-	if (retVal < 0)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
-	writeVal = write(STDOUT_FILENO, buffer, retVal);
-	if (writeVal < 0 || writeVal != retVal)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
-	free(buffer);
+	buf[s] = '\0';
+
 	close(fd);
-	return (retVal);
+
+	t = write(STDOUT_FILENO, buf, s);
+	if (t < 0)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	return (t);
 }
+
